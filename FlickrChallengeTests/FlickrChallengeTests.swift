6b11f11297d17991dd1ff1a0e4aeabf1405b106d
Marco Abundo <shrtlist@gmail.com>
2024-11-20 13:11:01 -0800

    Initial Commit

---
+++ FlickrChallengeTests/FlickrChallengeTests.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrChallengeTests.swift
+//  FlickrChallengeTests
+//
+//  Created by Marco Abundo on 11/20/24.
+//
+
+import Testing
+@testable import FlickrChallenge
+
+struct FlickrChallengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
